65e2c711a816ef5aaa62d4824ffe5843a77337c3
AdiKalavala24 <sportadi123@gmail.com>
2023-05-13 18:45:46 -0400

    new file:   ASL.css 	new file:   ASL.html 	modified:   components/footer.css 	new file:   components/footer.js 	modified:   components/head.css 	modified:   components/nav.css 	new file:   components/scrollbar.css 	new file:   files/Aux-Assist-Logo-Thumbnail.png 	new file:   files/Aux-Assist-Logo.png 	new file:   files/_space.png 	new file:   files/a.png 	new file:   files/b.png 	new file:   files/c.png 	new file:   files/d.png 	new file:   files/e.png 	new file:   files/f.png 	new file:   files/g.png 	new file:   files/h.png 	new file:   files/i.png 	new file:   files/j.png 	new file:   files/k.png 	new file:   files/l.png 	new file:   files/m.png 	new file:   files/n.png 	new file:   files/o.png 	new file:   files/p.png 	new file:   files/q.png 	new file:   files/r.png 	new file:   files/s.png 	new file:   files/t.png 	new file:   files/u.png 	new file:   files/v.png 	new file:   files/w.png 	new file:   files/x.png 	new file:   files/y.png 	new file:   files/z.png 	modified:   home.css 	modified:   home.html 	modified:   home.js 	new file:   learn-ASL.css 	new file:   learn-ASL.html 	new file:   learn-ASL.js

--- home.js
+++ home.js
@@ -34,4 +34,44 @@ function PopulateVoices(){
 
     voiceList.selectedIndex = selectedIndex;
 }
-var voice_input=document.querySelector("#voiceInput");
+var speak = document.getElementById('speak');
+var textarea = document.getElementById('textarea');
+var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+speak.addEventListener('click', function () {
+    recognition.start();
+    textarea.innerHTML = '...speaking';
+})
+recognition.onresult = function (e) {
+    var transcript = e.results[0][0].transcript;
+    textarea.innerHTML = transcript;
+}
+
+const audioContext = new AudioContext();
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(stream => {
+    const source = audioContext.createMediaStreamSource(stream);
+
+    const scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+    
+    source.connect(scriptNode);
+    
+    scriptNode.connect(audioContext.destination);
+    
+    scriptNode.onaudioprocess = function(event) {
+      const inputBuffer = event.inputBuffer.getChannelData(0);
+      let sum = 0;
+      for (let i = 0; i < inputBuffer.length; i++) {
+        sum += inputBuffer[i] * inputBuffer[i];
+      }
+      const rms = Math.sqrt(sum / inputBuffer.length);
+      const db = 20 * Math.log10(rms);
+      
+      // Update the decibel level meter
+      const meter = document.getElementById('meter');
+      const level = document.getElementById('level');
+      level.style.width = Math.max(0, Math.min(100, db + 100)) + '%';
+    };
+  })
+  .catch(err => console.error('Error accessing microphone:', err));
